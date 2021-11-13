759f23efe1bfe01b3d434f823059ad2081154d82
Romeo <kilazlove@gmail.com>
2021-11-13 21:37:26 +0800

    paste the assignment 2 then edit

--- index.py
+++ index.py
@@ -2,3 +2,8 @@
 # Redo the programs on assignment2 but now with functions that return multiple values 
 # (move all user inputs in one function)
 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+address = input("Enter your address: ")
+
+print("Hi, my name is " + name + ". I am "+ age +" years old and I live in " + address +".")
