a223382b625390a6fa17edd6f150b4e117274203
Romeo <kilazlove@gmail.com>
2021-11-13 22:00:01 +0800

    create functions that returns the value of variables, call the functions

--- index.py
+++ index.py
@@ -2,14 +2,30 @@
 # Redo the programs on assignment2 but now with functions that return multiple values 
 # (move all user inputs in one function)
 
+name = ""
+age = "" 
+address = ""
+
 def getInputs():
     global name
     global age
     global address
-    
+
     name = input("Enter your name: ")
     age = input("Enter your age: ")
     address = input("Enter your address: ")
 
+def printAll(name, age, address):
+    print("Hi, my name is " + name + ". I am "+ age +" years old and I live in " + address +".")
 
-print("Hi, my name is " + name + ". I am "+ age +" years old and I live in " + address +".")
+def getName():
+    return name
+
+def getAge():
+    return age
+
+def getAddress():
+    return address
+
+getInputs()
+printAll(getName(), getAge(), getAddress())
